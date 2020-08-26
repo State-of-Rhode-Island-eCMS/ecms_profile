904ae679828e0a0d38c5eff1c3e96a802365e184
Philip Frilling <pfrilling@oomphinc.com>
2020-08-26 10:17:54 -0400

    Fixed the clean config script to recurse properly

--- scripts/clean-config.sh
+++ scripts/clean-config.sh
@@ -9,24 +9,24 @@ BASE_DIR="$(dirname $(cd ${0%/*} && pwd))"
 echo -e "**********************"
 echo -e "*   Cleanup Config   *"
 echo -e "**********************"
-echo -e "Cleaning up configuration in: ${BASE_DIR}/config/install/\n"
-echo -e "Cleaning up configuration in: ${BASE_DIR}/modules/custom/*/config/install/\n"
+echo -e "Cleaning up configuration in: ${BASE_DIR}/*/config/install/\n"
+echo -e "Cleaning up configuration in: ${BASE_DIR}/*/modules/custom/*/config/install/\n"
 
 # sed is different for Macs, detect that here.
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  find ${BASE_DIR}/config/ -type f -name "*.yml" -exec sed -i '' '/^uuid: /d' {} \;
-  find ${BASE_DIR}/config/ -type f -name "*.yml" -exec sed -i '' '/_core:/{N;d;}' {} \;
+  find ${BASE_DIR}/*/config/ -type f -name "*.yml" -exec sed -i '' '/^uuid: /d' {} \;
+  find ${BASE_DIR}/*/config/ -type f -name "*.yml" -exec sed -i '' '/_core:/{N;d;}' {} \;
 
   # Traverse the custom modules too.
-  find ${BASE_DIR}/modules/custom -path "*/config/*.yml" -exec sed -i '' '/^uuid: /d' {} \;
-  find ${BASE_DIR}/modules/custom -path "*/config/*.yml" -exec sed -i '' '/_core:/{N;d;}' {} \;
+  find ${BASE_DIR}/*/modules/custom -path "*/config/*.yml" -exec sed -i '' '/^uuid: /d' {} \;
+  find ${BASE_DIR}/*/modules/custom -path "*/config/*.yml" -exec sed -i '' '/_core:/{N;d;}' {} \;
 else
-  find ${BASE_DIR}/config/install/ -type f -name "*.yml" -exec sed -i '/^uuid: /d' {} \;
-  find ${BASE_DIR}/config/install/ -type f -name "*.yml" -exec sed -i '/_core:/,+1d' {} \;
+  find ${BASE_DIR}/*/config/install/ -type f -name "*.yml" -exec sed -i '/^uuid: /d' {} \;
+  find ${BASE_DIR}/*/config/install/ -type f -name "*.yml" -exec sed -i '/_core:/,+1d' {} \;
 
   # Traverse the custom modules too.
-  find ${BASE_DIR}/modules/custom -path "*/config/*.yml" -exec sed -i '/^uuid: /d' {} \;
-  find ${BASE_DIR}/modules/custom -path "*/config/*.yml" -exec sed -i '/_core:/,+1d' {} \;
+  find ${BASE_DIR}/*/modules/custom -path "*/config/*.yml" -exec sed -i '/^uuid: /d' {} \;
+  find ${BASE_DIR}/*/modules/custom -path "*/config/*.yml" -exec sed -i '/_core:/,+1d' {} \;
 fi
 
 echo -e "********************************"
