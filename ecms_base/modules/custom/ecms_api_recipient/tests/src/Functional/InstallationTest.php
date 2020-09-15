6b65a497998e20a7ea91636e884e63fd12564a38
Philip Frilling <pfrilling@oomphinc.com>
2020-09-15 08:43:02 -0400

    Added a functional test to validate the install process

--- ecms_base/modules/custom/ecms_api_recipient/tests/src/Functional/InstallationTest.php
+++ ecms_base/modules/custom/ecms_api_recipient/tests/src/Functional/InstallationTest.php
@@ -40,12 +40,50 @@ class InstallationTest extends AllProfileInstallationTestsAbstract {
    * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function testEcmsApiRecipientInstallation(): void {
-    $account = $this->drupalCreateUser(['administer permissions']);
+    $account = $this->drupalCreateUser([
+      'administer permissions',
+      'administer consumer entities',
+      'administer users',
+    ]);
     $this->drupalLogin($account);
 
     // Ensure the ecms_api_recipient role was installed.
     $this->drupalGet('admin/people/roles/manage/ecms_api_recipient');
     $this->assertSession()->statusCodeEquals(200);
+
+    // Ensure the user account was created.
+    $this->drupalGet('admin/people');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->linkExists('ecms_api_recipient');
+    $this->clickLink('ecms_api_recipient');
+    $this->assertSession()->statusCodeEquals(200);
+    $accountUrl = $this->getUrl();
+
+    // Parse the url to get the user account id.
+    $accountDetails = explode('/', $accountUrl);
+    $accountId = $accountDetails[1];
+
+    // Browse to the user edit page.
+    $this->drupalGet("{$accountUrl}/edit");
+    // Ensure the user has the correct role selected.
+    $this->assertSession()->checkboxChecked('edit-roles-ecms-api-recipient');
+    // Ensure the user is active.
+    $this->assertSession()->checkboxChecked('edit-status-1');
+
+    // Assert the consumer is in the consumer's list.
+    $this->drupalGet('admin/config/services/consumer');
+    $this->assertSession()->statusCodeEquals(200);
+    // Ensure the created consumer exists.
+    $this->assertSession()->linkExists('eCMS Recipient');
+    $this->clickLink('eCMS Recipient');
+    $url = $this->getUrl();
+    // Edit the consumer.
+    $this->drupalGet("{$url}/edit");
+    $this->assertSession()->statusCodeEquals(200);
+    // Ensure the api user is set.
+    $this->assertSession()->fieldValueEquals('edit-user-id-0-target-id', "ecms_api_recipient ({$accountId})");
+    // Ensure the correct role is set.
+    $this->assertSession()->checkboxChecked('edit-roles-ecms-api-recipient');
   }
 
 }
