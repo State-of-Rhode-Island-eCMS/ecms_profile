39a3b8d7059a205f395f464f5e7a270fa908d542
Ben Hamelin <bhamelin@oomphinc.com>
2020-09-29 15:02:57 -0400

    Update class comment

--- ecms_base/features/custom/ecms_publications/tests/src/Functional/EcmsPublicationsInstallTest.php
+++ ecms_base/features/custom/ecms_publications/tests/src/Functional/EcmsPublicationsInstallTest.php
@@ -10,7 +10,7 @@ require_once dirname(__FILE__) . '/../../../../../../../tests/src/Functional/All
 use Drupal\Tests\ecms_profile\Functional\AllProfileInstallationTestsAbstract;
 
 /**
- * Class InstallationTest.
+ * Class EcmsPublicationsInstallTest.
  *
  * @package Drupal\Tests\ecms_publications\Functional
  * @group ecms
