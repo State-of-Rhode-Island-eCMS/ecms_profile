8b79410b5f46747d217b3d730b61ddb3a509b73d
Philip Frilling <pfrilling@oomphinc.com>
2020-10-07 21:27:05 -0400

    Changed to the existingsite test

--- ecms_base/features/custom/ecms_executive_orders/tests/src/ExistingSite/EcmsExecutiveOrdersInstallTest.php
+++ ecms_base/features/custom/ecms_executive_orders/tests/src/ExistingSite/EcmsExecutiveOrdersInstallTest.php
@@ -5,14 +5,14 @@ declare(strict_types = 1);
 namespace Drupal\Tests\ecms_executive_orders\Functional;
 
 // Require the all profiles abstract class since autoloading doesn't work.
-require_once dirname(__FILE__) . '/../../../../../../../tests/src/Functional/AllProfileInstallationTestsAbstract.php';
+require_once dirname(__FILE__) . '/../../../../../../../tests/src/ExistingSite/AllProfileInstallationTestsAbstract.php';
 
-use Drupal\Tests\ecms_profile\Functional\AllProfileInstallationTestsAbstract;
+use Drupal\Tests\ecms_profile\ExistingSite\AllProfileInstallationTestsAbstract;
 
 /**
  * Functional tests for the EcmsExecutiveOrdersInstall feature.
  *
- * @package Drupal\Tests\ecms_executive_orders\Functional
+ * @package Drupal\Tests\ecms_executive_orders\ExistingSite
  * @group ecms
  * @group ecms_executive_orders
  */
@@ -38,7 +38,7 @@ class EcmsExecutiveOrdersInstallTest extends AllProfileInstallationTestsAbstract
    * @throws \Behat\Mink\Exception\ExpectationException
    */
   public function testEcmsExecutiveOrdersInstallation(): void {
-    $account = $this->drupalCreateUser([
+    $account = $this->createUser([
       'administer modules',
       'administer site configuration',
       'access administration pages',
@@ -53,7 +53,7 @@ class EcmsExecutiveOrdersInstallTest extends AllProfileInstallationTestsAbstract
     $edit = [];
     $edit["modules[ecms_executive_orders][enable]"] = TRUE;
     $this->drupalPostForm(NULL, $edit, t('Install'));
-    $this->assertText('Module eCMS Executive Orders has been enabled.');
+    $this->assertSession()->pageTextContainsOnce('Module eCMS Executive Orders has been enabled.');
 
   }
 
