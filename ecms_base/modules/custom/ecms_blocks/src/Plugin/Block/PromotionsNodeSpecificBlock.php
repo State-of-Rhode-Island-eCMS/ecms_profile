5a2943533b867c33ff02acd5e91cb2499b7d7820
Jordan Caldwell <jcaldwell@oomphinc.com>
2020-11-04 14:16:40 -0500

    Rework node specific promotions block to use ContainerInterface

--- ecms_base/modules/custom/ecms_blocks/src/Plugin/Block/PromotionsNodeSpecificBlock.php
+++ ecms_base/modules/custom/ecms_blocks/src/Plugin/Block/PromotionsNodeSpecificBlock.php
@@ -6,8 +6,12 @@ namespace Drupal\ecms_blocks\Plugin\Block;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\node\NodeInterface;
 
 /**
  * Provides a listing of promos referenced by the node.
@@ -20,7 +24,66 @@ use Drupal\Core\Session\AccountInterface;
  *   }
  * )
  */
-class PromotionsNodeSpecificBlock extends BlockBase {
+class PromotionsNodeSpecificBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The account service.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $account;
+
+  /**
+   * The language_manager service.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * The entity_type.manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('language_manager'),
+      $container->get('entity_type.manager'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * PromotionsNodeSpecificBlock constructor.
+   *
+   * @param array $configuration
+   *   Configuration array for the block.
+   * @param string $plugin_id
+   *   The plugin id.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
+   *   The language_manager service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity_type.manager service.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The entity_type.manager service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LanguageManagerInterface $languageManager, EntityTypeManagerInterface $entityTypeManager, AccountInterface $account) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->languageManager = $languageManager;
+    $this->entityTypeManager = $entityTypeManager;
+    $this->account = $account;
+  }
 
   /**
    * {@inheritdoc}
@@ -28,54 +91,38 @@ class PromotionsNodeSpecificBlock extends BlockBase {
   public function build(): array {
     // Load node from context.
     $node = $this->getContextValue('node');
+    if (!$node instanceof NodeInterface) {
+      return [];
+    }
 
     // Assume what the promotions field machine name is.
     $field_string_guess = 'field_' . $node->bundle() . '_promotions';
 
-    // Using a switch in case an entity doesn't follow the above pattern.
-    // Set $promos to an array of promotion entities.
-    switch (TRUE) {
-      case ($node->hasField($field_string_guess)):
-        $promos = $node->get($field_string_guess)->referencedEntities();
-        break;
-
-      default:
-        $promos = NULL;
+    // Guard against a missing field.
+    if (!$node->hasField($field_string_guess)) {
+      return [];
     }
 
-    // If there are promos, render their teaser view.
-    if (count($promos) > 0) {
-      // Get language code.
-      $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $promos = $node->get($field_string_guess)->referencedEntities();
 
-      // Return a list of rendered teaser nodes.
-      $builder = \Drupal::entityTypeManager()->getViewBuilder('node');
-      return $builder->viewMultiple($promos, 'teaser', $language);
+    // Guard against no promos.
+    if (empty($promos)) {
+      return [];
     }
 
-    // If there is not a map file do not render block.
-    return [];
+    // Get language code.
+    $language = $this->languageManager->getCurrentLanguage()->getId();
+
+    // Return a list of rendered teaser nodes.
+    $builder = $this->entityTypeManager->getViewBuilder('node');
+    return $builder->viewMultiple($promos, 'teaser');
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function blockAccess(AccountInterface $account): object {
-    return AccessResult::allowedIfHasPermission($account, 'access content');
+  protected function blockAccess(): AccessResult {
+    return AccessResult::allowedIfHasPermission($this->account, 'access content');
   }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockForm($form, FormStateInterface $form_state): array {
-    $config = $this->getConfiguration();
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockSubmit($form, FormStateInterface $form_state): void {}
 
 }
