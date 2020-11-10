80055ef4a0a3adeeb122c3df79591f4180315186
Philip Frilling <pfrilling@oomphinc.com>
2020-11-10 14:06:23 -0500

    Added unit tests for the helper class

--- ecms_base/modules/custom/ecms_api/src/EcmsApiHelper.php
+++ ecms_base/modules/custom/ecms_api/src/EcmsApiHelper.php
@@ -7,17 +7,52 @@ namespace Drupal\ecms_api;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\StreamWrapper\PublicStream;
 
+/**
+ * Helper service for the ecms_api base class.
+ *
+ * @package Drupal\ecms_api
+ */
 class EcmsApiHelper {
 
+  /**
+   * The entity_type.manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
   protected $entityTypeManager;
 
+  /**
+   * The stream_wrapper.public service.
+   *
+   * @var \Drupal\Core\StreamWrapper\PublicStream
+   */
   private $publicStreamWrapper;
 
+  /**
+   * EcmsApiHelper constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity_type.manager service.
+   * @param \Drupal\Core\StreamWrapper\PublicStream $publicStream
+   *   The stream_wrapper.public service.
+   */
   public function __construct(EntityTypeManagerInterface $entityTypeManager, PublicStream $publicStream) {
     $this->entityTypeManager = $entityTypeManager;
     $this->publicStreamWrapper = $publicStream;
   }
 
+  /**
+   * Get the file path from file entity id.
+   *
+   * @param int $sourceFile
+   *   The id of a file entity.
+   *
+   * @return string|null
+   *   The real path to the file or null.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
   public function getFilePath(int $sourceFile): ?string {
     $storage = $this->entityTypeManager->getStorage('file');
     /** @var \Drupal\file\FileInterface $file */
@@ -28,7 +63,7 @@ class EcmsApiHelper {
       return NULL;
     }
 
-    // @todo: Get the realpath to the file.
+    // Get the uri to the file.
     $path = $file->getFileUri();
 
     $host = parse_url($path, PHP_URL_HOST);
@@ -36,16 +71,6 @@ class EcmsApiHelper {
     $realpath = $this->publicStreamWrapper->realpath();
 
     return "{$realpath}/{$host}{$path}";
-  }
-
-  private function getRealPath(string $uri): ?string {
-    $path = $this->publicStreamWrapper->realpath();
-
-    if (empty($path)) {
-      return NULL;
-    }
-
-    return $path;
   }
 
 }
---
+++ ecms_base/modules/custom/ecms_api/tests/src/Unit/EcmsApiHelperTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\ecms_api\Unit;
+
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StreamWrapper\PublicStream;
+use Drupal\ecms_api\EcmsApiHelper;
+use Drupal\file\FileInterface;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Unit tests for the EcmsApiHelper class.
+ *
+ * @package Drupal\Tests\ecms_api\Unit
+ * @group ecms_api
+ */
+class EcmsApiHelperTest extends UnitTestCase {
+
+  /**
+   * A static file id to test with.
+   */
+  const FILE_ID = 2020;
+
+  /**
+   * Mock of the entity_type.manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface|\PHPUnit\Framework\MockObject\MockObject
+   */
+  private $entityTypeManager;
+
+  /**
+   * Mock of the stream_wrapper.public service.
+   *
+   * @var \Drupal\Core\StreamWrapper\PublicStream|\PHPUnit\Framework\MockObject\MockObject
+   */
+  private $publicStreamWrapper;
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->entityTypeManager = $this->createMock(EntityTypeManagerInterface::class);
+    $this->publicStreamWrapper = $this->createMock(PublicStream::class);
+  }
+
+  /**
+   * Test the getFilePath method.
+   *
+   * @param string|null $expected
+   *   The string to test with or null.
+   *
+   * @dataProvider dataProviderForTestGetFilePath
+   */
+  public function testGetFilePath(?string $fileUri, ?string $expected): void {
+    $file = $expected;
+
+    if (!empty($expected)) {
+      $file = $this->createMock(FileInterface::class);
+      $file->expects($this->once())
+        ->method('getFileUri')
+        ->willReturn($fileUri);
+
+      $realpath = "/var/www/html/sites/default/files";
+      $this->publicStreamWrapper->expects($this->once())
+        ->method('realpath')
+        ->willReturn($realpath);
+    }
+
+    $fileStorage = $this->createMock(EntityStorageInterface::class);
+    $fileStorage->expects($this->once())
+      ->method('load')
+      ->with(self::FILE_ID)
+      ->willReturn($file);
+
+    $this->entityTypeManager->expects($this->once())
+      ->method('getStorage')
+      ->with('file')
+      ->willReturn($fileStorage);
+
+    $testClass = new EcmsApiHelper($this->entityTypeManager, $this->publicStreamWrapper);
+    $actual = $testClass->getFilePath(self::FILE_ID);
+
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * Data provider for the testGetFilePath method.
+   *
+   * @return array
+   *   Array of parameters to pass to the testGetFilePath method.
+   */
+  public function dataProviderForTestGetFilePath(): array {
+    return [
+      'test1' => [NULL, NULL],
+      'test2' => [
+        'public://filename.png',
+        '/var/www/html/sites/default/files/filename.png',
+      ],
+      'test3' => [
+        'public://path/to/file/in/sites/directory/filename.png',
+        '/var/www/html/sites/default/files/path/to/file/in/sites/directory/filename.png',
+      ],
+    ];
+  }
+
+}
