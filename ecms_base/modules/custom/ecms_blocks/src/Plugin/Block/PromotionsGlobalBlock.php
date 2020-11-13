116cb0a577a2e2bfa3f87a96c8df720aa1e58e31
Jordan Caldwell <jcaldwell@oomphinc.com>
2020-11-13 13:00:26 -0500

    Place global notification in custom black rather than view

--- ecms_base/modules/custom/ecms_blocks/src/Plugin/Block/PromotionsGlobalBlock.php
+++ ecms_base/modules/custom/ecms_blocks/src/Plugin/Block/PromotionsGlobalBlock.php
@@ -13,7 +13,7 @@ use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
- * Provides a listing of global promos.
+ * Provides a listing of promos referenced by the node.
  *
  * @Block(
  *   id = "ecms_promotions_global",
@@ -50,7 +50,7 @@ class PromotionsGlobalBlock extends BlockBase implements ContainerFactoryPluginI
   }
 
   /**
-   * PromotionsGlobalBlock constructor.
+   * PromotionsNodeSpecificBlock constructor.
    *
    * @param array $configuration
    *   Configuration array for the block.
