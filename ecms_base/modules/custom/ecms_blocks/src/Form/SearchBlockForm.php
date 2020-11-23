3a47c04884b285a74a13f48b7b785bdf1698bc66
Jordan Caldwell <jcaldwell@oomphinc.com>
2020-11-23 10:16:33 -0500

    Search block form theming

---
+++ ecms_base/modules/custom/ecms_blocks/src/Form/SearchBlockForm.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\ecms_blocks\Form;
+
+/**
+ * @file
+ * Contains \Drupal\ecms_blocks\Form\SearchBlockForm.
+ */
+
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+class SearchBlockForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'ecms_search_block_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['#theme'] = 'ecms_search_block_form';
+
+    $form['search_input'] = array(
+      '#type' => 'textfield',
+      "#placeholder" => t('Search the site'),
+      '#required' => TRUE,
+      "#attributes" => [
+        'type' => "search"
+      ],
+      '#theme_wrappers' => array()
+    );
+
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Register'),
+      '#button_type' => 'primary',
+    );
+
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $input = $form_state->getValue('search_input');
+    $params['query'] = [
+        'q' => $input,
+    ];
+    $form_state->setRedirectUrl(Url::fromUri('internal:' . '/search', $params));
+  }
+}
---
+++ ecms_base/modules/custom/ecms_blocks/src/Plugin/Block/SearchBlock.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\ecms_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+
+
+/**
+ * Provides a listing of promos referenced by the node.
+ *
+ * @Block(
+ *   id = "ecms_search_block",
+ *   admin_label = @Translation("Search Block"),
+ * )
+ */
+class SearchBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Form builder service.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $form_builder;
+
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * PromotionsNodeSpecificBlock constructor.
+   *
+   * @param array $configuration
+   *   Configuration array for the block.
+   * @param string $plugin_id
+   *   The plugin id.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The Form builder service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->form_builder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    // $form = $this->formBuilder->getForm('Drupal\ecms_blocks\Form\SearchBlockForm');
+
+    $output['form'] = $this->form_builder->getForm('Drupal\ecms_blocks\Form\SearchBlockForm');
+    return $output;
+
+    // return $form;
+    return [];
+  }
+
+}
