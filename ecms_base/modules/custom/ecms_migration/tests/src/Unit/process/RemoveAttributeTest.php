181b33a4b96a89851d29aae7b34c23d33915997a
Ben Hamelin <bhamelin@oomphinc.com>
2021-01-11 14:02:58 -0500

    Add multiple style test

--- ecms_base/modules/custom/ecms_migration/tests/src/Unit/process/RemoveAttributeTest.php
+++ ecms_base/modules/custom/ecms_migration/tests/src/Unit/process/RemoveAttributeTest.php
@@ -28,6 +28,20 @@ class RemoveAttributeTest extends MigrateProcessTestCase {
   }
 
   /**
+   * Test for a removal of all style attributes.
+   */
+  public function testRemoveMultipleStyles(): void {
+    $value = '<div style="width:500px;">Content to leave</div><p style="color:red;">Content to leave</p><span style="color:blue; font-size: 10px;">Content to leave</span>';
+    $configuration['attribute'] = 'style';
+    $plugin = new RemoveAttribute($configuration, 'remove_attribute', []);
+    $actual = $plugin->transform($value, $this->migrateExecutable, $this->row, 'destinationproperty');
+    $this->assertStringNotContainsString('style="width:500px;"', $actual);
+    $this->assertStringNotContainsString('style="color:red;"', $actual);
+    $this->assertStringNotContainsString('style="color:blue; font-size: 10px;"', $actual);
+
+  }
+
+  /**
    * Test for not removing a class attribute.
    */
   public function testIgnoreAttribute(): void {
