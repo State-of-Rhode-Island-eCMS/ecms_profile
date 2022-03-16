2f23e00eab69c4fd0db836599cd00cb0aa69b452
Ben Hamelin <bhamelin@oomphinc.com>
2022-03-16 15:52:34 -0400

    Update core to 9.3.8

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -16,7 +16,7 @@ PATTERN_LAB_REPOSITORY_NAME="state-of-rhode-island-ecms/ecms_patternlab"
 COMPOSER="$(which composer)"
 COMPOSER_BIN_DIR="$(composer config bin-dir)"
 DOCROOT="web"
-DRUPAL_CORE_VERSION="9.3.7"
+DRUPAL_CORE_VERSION="9.3.8"
 
 # Whether the source directory should be deleted before rebuilding lando
 DELETE_SRC=0
