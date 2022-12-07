a09dce1c1a24d2562111cf4642a23ed7e95e7a98
swiftwinning <jwinning@oomphinc.com>
2022-12-07 15:13:58 -0500

    Debug the debugger statement yet more

--- ecms_base/modules/custom/ecms_icon_library/src/Plugin/Field/FieldFormatter/EcmsIconLibraryFormatter.php
+++ ecms_base/modules/custom/ecms_icon_library/src/Plugin/Field/FieldFormatter/EcmsIconLibraryFormatter.php
@@ -62,10 +62,11 @@ class EcmsIconLibraryFormatter extends FormatterBase {
         $message = "FID: " . $fid . "\n";
         $message = $message . "file_uri: " . $file_uri . "\n";
         $message = $message . "file_url: " . $file_url . "\n";
+        $message = $message . "svgRaw is string?: " . is_string($svgRaw) . "\n";
         $message = $message . "substr: " . substr($svgRaw, 0, 14) . "\n";
         $message = $message . "substr === Page not found?: " . (substr($svgRaw, 0, 14) === "Page not found") . "\n";
-        $message = $message . "svgRaw: " . $svgRaw . "\n";
-        $message = $message . "svgRaw: " . $svgRaw . "\n";
+        // $message = $message . "svgRaw: " . $svgRaw . "\n";
+        // $message = $message . "svgRaw: " . $svgRaw . "\n";
         if (substr($svgRaw, 0, 14) === "Page not found") {
           \Drupal::logger('my_module')->notice("FID: " . $fid . " can't be loaded as SVG.");
           return;
