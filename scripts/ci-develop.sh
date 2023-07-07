bd5344c3624ffc909aa5c2f6c6205d4edab3c4a4
swiftwinning <jwinning@oomphinc.com>
2023-07-06 23:51:26 -0400

    Update DRUPAL_CORE_VERSION constant 9.4.8=>9.5.9 in ci-develop.sh

--- scripts/ci-develop.sh
+++ scripts/ci-develop.sh
@@ -15,7 +15,7 @@ PATTERN_LAB_REPOSITORY_NAME="state-of-rhode-island-ecms/ecms_patternlab"
 COMPOSER="$(which composer)"
 COMPOSER_BIN_DIR="$(composer config bin-dir)"
 DOCROOT="web"
-DRUPAL_CORE_VERSION="9.4.8"
+DRUPAL_CORE_VERSION="9.5.9"
 
 # Move up a directory.
 cd ..
