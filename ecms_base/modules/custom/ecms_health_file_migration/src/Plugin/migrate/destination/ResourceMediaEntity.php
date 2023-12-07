ed3e3e85794c1dfdb50adb1db8f96f6cc63c1bf0
swiftwinning <jwinning@oomphinc.com>
2023-12-07 05:55:55 -0500

    Restore custom create function

--- ecms_base/modules/custom/ecms_health_file_migration/src/Plugin/migrate/destination/ResourceMediaEntity.php
+++ ecms_base/modules/custom/ecms_health_file_migration/src/Plugin/migrate/destination/ResourceMediaEntity.php
@@ -147,25 +147,25 @@ class ResourceMediaEntity extends EntityContentBase {
 //     }
 //     $this->accountSwitcher = $account_switcher;
 //   }
-//
-//   /**
-//    * {@inheritdoc}
-//    */
-//   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
-//     $entity_type = static::getEntityTypeId($plugin_id);
-//     return new static(
-//       $configuration,
-//       $plugin_id,
-//       $plugin_definition,
-//       $migration,
-//       $container->get('entity_type.manager')->getStorage($entity_type),
-//       array_keys($container->get('entity_type.bundle.info')->getBundleInfo($entity_type)),
-//       $container->get('entity_field.manager'),
-//       $container->get('plugin.manager.field.field_type'),
-//       $container->get('account_switcher')
-//     );
-//   }
-//
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, 'media', 'resource_media_destination', MigrationInterface $migration = NULL) {
+    $entity_type = static::getEntityTypeId($plugin_id);
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $migration,
+      $container->get('entity_type.manager')->getStorage($entity_type),
+      array_keys($container->get('entity_type.bundle.info')->getBundleInfo($entity_type)),
+      $container->get('entity_field.manager'),
+      $container->get('plugin.manager.field.field_type'),
+      $container->get('account_switcher')
+    );
+  }
+
 //   /**
 //    * {@inheritdoc}
 //    *
