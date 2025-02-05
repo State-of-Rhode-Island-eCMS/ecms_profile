916308580aecfa9d053dd87079c1422a163b80b5
Phil Frilling <pfrilling@oomphinc.com>
2025-02-05 14:43:29 -0500

    Updated docblock"

--- ecms_base/modules/custom/ecms_api_recipient/src/Drush/Commands/RegisterWithHubCommand.php
+++ ecms_base/modules/custom/ecms_api_recipient/src/Drush/Commands/RegisterWithHubCommand.php
@@ -26,17 +26,28 @@ final class RegisterWithHubCommand extends DrushCommands {
   }
 
   /**
-   * Sync Aptify products to Drupal entities.
+   * Register the site with the main hub.
+   *
+   * @command ecms:register-hub
+   * @aliases ecms:rh
+   *
+   * @usage ons:register-hub
+   *   Register the site with the main hub.
    */
-  #[Command(name: 'ecms:register-hub', aliases: ['ecms:rh'])]
-  #[Usage(name: 'ecms:register-hub', description: 'Register the site with the main hub.')]
   public function registerWithHub(): void {
     // Register with the hub site.
     $this->apiRecipientRegister->registerWithHub();
   }
 
-  #[Command(name: 'ecms:get-hub-content', aliases: ['ecms:ghc'])]
-  #[Usage(name: 'ecms:get-hub-content', description: 'Retrieve the content from the hub.')]
+  /**
+   * Retrieve content from the main hub.
+   *
+   * @command ecms:get-hub-content
+   * @aliases ecms:ghc
+   *
+   * @usage ons:get-hub-content
+   *   Get all content that should be on the site from the hub.
+   */
   public function retieveHubContent(): void {
     // Register with the hub site.
     $this->apiRecipientRetrieveNotifications->retrieveHubContent();
