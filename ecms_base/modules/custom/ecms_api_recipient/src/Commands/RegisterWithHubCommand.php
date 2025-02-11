80c8a8813f8bc57167931e624a660a025ea69d7a
Phil Frilling <pfrilling@oomphinc.com>
2025-02-11 09:27:43 -0500

    Updated script

--- ecms_base/modules/custom/ecms_api_recipient/src/Commands/RegisterWithHubCommand.php
+++ ecms_base/modules/custom/ecms_api_recipient/src/Commands/RegisterWithHubCommand.php
@@ -7,7 +7,9 @@ namespace Drupal\ecms_api_recipient\Commands;
 use Drupal\ecms_api_recipient\EcmsApiRecipientRegister;
 use Drupal\ecms_api_recipient\EcmsApiRecipientRetrieveNotifications;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\FileStorage
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleExtensionList;
 
 use Drush\Commands\DrushCommands;
 
@@ -24,6 +26,8 @@ final class RegisterWithHubCommand extends DrushCommands {
     private readonly EcmsApiRecipientRetrieveNotifications $apiRecipientRetrieveNotifications,
     private readonly EntityTypeManagerInterface $entityTypeManager,
     private readonly ConfigFactoryInterface $configFactory,
+    private readonly ModuleExtensionList $moduleExtensionList,
+    private readonly StorageCacheInterface $storageCache,
   ) {
     parent::__construct();
   }
@@ -57,13 +61,36 @@ final class RegisterWithHubCommand extends DrushCommands {
     $role = $this->entityTypeManager->getStorage('user_role')->load('ecms_api_recipient');
     if (!$role) {
       $this->logger()->error('The ecms_api_recipient role does not exist.');
-      return;
+      $this->installEcmsApiRecipientRole();
+      // Reload the role
+      $role = $this->entityTypeManager->getStorage('user_role')->load('ecms_api_recipient');
+      if (!$role) {
+        $this->logger()
+          ->error('The ecms_api_recipient role could not be installed.');
+        return;
+      }
+
+      $member = $this->entityTypeManager->getStorage('user')->loadByProperties([
+        'name' => 'ecms_api_recipient',
+      ]);
+
+      $member->addRole('ecms_api_recipient');
+      $member->save();
     }
 
     $role->grantPermission("create emergency_notification content");
     $role->grantPermission("edit own emergency_notification content");
     $role->grantPermission("translate emergency_notification node");
     $role->save();
+  }
+
+  /**
+   * Install the ecms_api_recipient role from the install directory.
+   */
+  private function installEcmsApiRecipientRole(): void {
+    $path = $this->moduleExtensionList->getPath('ecms_api_recipient');
+    $install_source = new FileStorage($path . "/config/install/");
+    $this->storageCache->write('user.role.ecms_api_recipient', $install_source->read('user.role.ecms_api_recipient'));
   }
 
   /**
