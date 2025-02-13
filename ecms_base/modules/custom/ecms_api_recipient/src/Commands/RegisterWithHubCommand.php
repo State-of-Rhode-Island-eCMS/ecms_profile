1da651d498fed5227a47eb6a4b408823e4f5ab64
Phil Frilling <pfrilling@oomphinc.com>
2025-02-13 09:04:36 -0500

    Fixed linting error

--- ecms_base/modules/custom/ecms_api_recipient/src/Commands/RegisterWithHubCommand.php
+++ ecms_base/modules/custom/ecms_api_recipient/src/Commands/RegisterWithHubCommand.php
@@ -48,8 +48,7 @@ final class RegisterWithHubCommand extends DrushCommands {
   }
 
   /**
-   * Grant the ecms_api_recipient role access to syndicate
-   * emergency notifications.
+   * Grant the recipient role access to syndicate emergency notifications.
    *
    * @command ecms:grant-emergency-notification-access
    * @aliases ecms:gena
@@ -64,7 +63,10 @@ final class RegisterWithHubCommand extends DrushCommands {
       $this->logger()->error('The ecms_api_recipient role does not exist.');
       $this->installEcmsApiRecipientRole();
       // Reload the role
-      $role = $this->entityTypeManager->getStorage('user_role')->load('ecms_api_recipient');
+      $role = $this->entityTypeManager
+        ->getStorage('user_role')
+        ->load('ecms_api_recipient');
+
       if (!$role) {
         $this->logger()
           ->error('The ecms_api_recipient role could not be installed.');
