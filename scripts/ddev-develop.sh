ec4c7fcbf95744379e1a23da4a42baf0279f8a6e
Phil Frilling <pfrilling@oomphinc.com>
2025-04-16 10:50:38 -0400

    DDEV Switch

--- scripts/ddev-develop.sh
+++ scripts/ddev-develop.sh
@@ -7,6 +7,8 @@ DRUPAL_CORE="${DRUPAL_CORE:-^10.4}"
 PHP_VERSION="${PHP_VERSION:-8.3}"
 APP_NAME="ecms-profile"
 DOCROOT="develop/web"
+PATTERN_LAB_DIRECTORY="ecms_patternlab"
+PATTERN_LAB_REPOSITORY_NAME="state-of-rhode-island-ecms/ecms_patternlab"
 
 # Make sure the system has ddev
 if ! [ -x "$(command -v ddev)" ]; then
@@ -34,22 +36,23 @@ ddev exec 'rm -Rf develop'
 ddev exec "composer create-project drupal/recommended-project:${DRUPAL_CORE} develop --no-install"
 # Delete the lock file to add the profile's dependencies.
 ddev exec 'rm -f develop/composer.lock'
+
 # Symlink the profile
-# https://github.com/ddev/ddev/pull/7199 DDEV needs a release for the DDEV_APPROOT variable to exist.
-# Create a `custom` profile directory
-ddev exec 'mkdir -p $DDEV_DOCROOT/profiles/custom/ecms_profile'
+# Create a `contrib` profile directory
+ddev exec 'mkdir -p $DDEV_DOCROOT/profiles/contrib/ecms_profile'
 
 # Symlink the base profiles into the `ecms_profile` profile directory.
-ddev exec 'ln -s -f $(realpath -s --relative-to=${DDEV_DOCROOT}/profiles/custom/ecms_profile ecms_base) $DDEV_DOCROOT/profiles/custom/ecms_profile'
-ddev exec 'ln -s -f $(realpath -s --relative-to=${DDEV_DOCROOT}/profiles/custom/ecms_profile ecms_acquia) $DDEV_DOCROOT/profiles/custom/ecms_profile'
+ddev exec 'ln -s -f $(realpath -s --relative-to=${DDEV_DOCROOT}/profiles/contrib/ecms_profile ecms_base) $DDEV_DOCROOT/profiles/contrib/ecms_profile'
+ddev exec 'ln -s -f $(realpath -s --relative-to=${DDEV_DOCROOT}/profiles/contrib/ecms_profile ecms_acquia) $DDEV_DOCROOT/profiles/contrib/ecms_profile'
 
 ## Merge the profile's composer into Drupal's default.
 ddev exec "test -f develop/composer.lock || (generate-composer > develop/merge.composer.json)"
 
 ## If the merge was successful, replace the original composer.json and install.
-ddev exec 'test -f develop/merge.composer.json && (mv develop/merge.composer.json develop/composer.json && composer install --working-dir=develop)'
+ddev exec 'test -f develop/merge.composer.json && (mv develop/merge.composer.json develop/composer.json)'
 
-## Run composer install in the web d
+# Composer install the things.
+ddev exec 'composer install --working-dir=develop'
+
+## Install the site profile.
 ddev exec 'drush site:install ecms_acquia --db-url=mysql://db:db@db:3306/db?module=mysql#tableprefix --yes'
-
-
