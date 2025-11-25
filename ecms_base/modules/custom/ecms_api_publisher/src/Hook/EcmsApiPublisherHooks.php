ce4735dcf0ae5c3c7ee48584dccc11011f14d638
Phil Frilling <pfrilling@oomphinc.com>
2025-11-25 09:49:32 -0500

    Hashed the client secrets

--- ecms_base/modules/custom/ecms_api_publisher/src/Hook/EcmsApiPublisherHooks.php
+++ ecms_base/modules/custom/ecms_api_publisher/src/Hook/EcmsApiPublisherHooks.php
@@ -63,10 +63,11 @@ class EcmsApiPublisherHooks {
         ->get('oauth_client_id')
       );
 
-      $consumer->set('secret', $this->configFactory
-        ->get('ecms_api_publisher.settings')
-        ->get('oauth_client_secret')
-      );
+      $consumer->set('secret', $this->password->hash(
+        $this->configFactory
+          ->get('ecms_api_publisher.settings')
+          ->get('oauth_client_secret')
+      ));
     }
   }
 
