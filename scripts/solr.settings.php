96b6524de2dc81dcb08222c167fd6fba2cb29ac8
Phil Frilling <pfrilling@oomphinc.com>
2026-04-24 11:22:23 -0400

    Added Solr to the local environment

---
+++ scripts/solr.settings.php
@@ -0,0 +1,13 @@
+<?php
+
+// Search API
+$config['search_api.server.acquia_search_server']['backend'] = 'search_api_solr';
+$config['search_api.server.acquia_search_server']['backend_config']['connector'] = 'solr_cloud_basic_auth';
+$config['search_api.server.acquia_search_server']['backend_config']['connector_config']['scheme'] = 'http';
+$config['search_api.server.acquia_search_server']['backend_config']['connector_config']['host'] = 'solr';
+$config['search_api.server.acquia_search_server']['backend_config']['connector_config']['port'] = '8983';
+$config['search_api.server.acquia_search_server']['backend_config']['connector_config']['path'] = '/';
+$config['search_api.server.acquia_search_server']['backend_config']['connector_config']['core'] = 'ecms';
+$config['search_api.server.acquia_search_server']['backend_config']['connector_config']['context'] = 'solr';
+$config['search_api.server.acquia_search_server']['backend_config']['connector_config']['username'] = 'solr';
+$config['search_api.server.acquia_search_server']['backend_config']['connector_config']['password'] = 'SolrRocks';
