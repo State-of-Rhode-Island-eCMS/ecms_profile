13ab8fdf8ae48eb05c0a3fcf2d7e0c957232f411
Philip Frilling <pfrilling@oomphinc.com>
2026-04-30 09:45:24 -0400

    Setup multisite recipe, press release theming (#698)
    * Setup multisite recipe, press release theming
    
    * Updated rigov recipe and solr config
    
    * Linting fixes

--- scripts/solr.settings.php
+++ scripts/solr.settings.php
@@ -11,3 +11,14 @@ $config['search_api.server.acquia_search_server']['backend_config']['connector_c
 $config['search_api.server.acquia_search_server']['backend_config']['connector_config']['context'] = 'solr';
 $config['search_api.server.acquia_search_server']['backend_config']['connector_config']['username'] = 'solr';
 $config['search_api.server.acquia_search_server']['backend_config']['connector_config']['password'] = 'SolrRocks';
+
+$config['search_api.server.ecms_multisite_server']['backend'] = 'search_api_solr';
+$config['search_api.server.ecms_multisite_server']['backend_config']['connector'] = 'solr_cloud_basic_auth';
+$config['search_api.server.ecms_multisite_server']['backend_config']['connector_config']['scheme'] = 'http';
+$config['search_api.server.ecms_multisite_server']['backend_config']['connector_config']['host'] = 'solr';
+$config['search_api.server.ecms_multisite_server']['backend_config']['connector_config']['port'] = '8983';
+$config['search_api.server.ecms_multisite_server']['backend_config']['connector_config']['path'] = '/';
+$config['search_api.server.ecms_multisite_server']['backend_config']['connector_config']['core'] = 'ecms';
+$config['search_api.server.ecms_multisite_server']['backend_config']['connector_config']['context'] = 'solr';
+$config['search_api.server.ecms_multisite_server']['backend_config']['connector_config']['username'] = 'solr';
+$config['search_api.server.ecms_multisite_server']['backend_config']['connector_config']['password'] = 'SolrRocks';
