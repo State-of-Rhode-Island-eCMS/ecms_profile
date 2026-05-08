b0af708b520efa842106c34409980c812e2c0590
Phil Frilling <pfrilling@oomphinc.com>
2026-05-08 13:55:25 -0400

    Added searchstax

---
+++ scripts/solr.settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$config['search_api.server.searchstax']['backend_config']['connector_config']['update_endpoint'] = getenv('SEARCHSTAX_UPDATE_ENDPOINT');
+$config['search_api.server.searchstax']['backend_config']['connector_config']['update_token'] = getenv('SEARCHSTAX_UPDATE_TOKEN');
